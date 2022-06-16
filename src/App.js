e988c52b7a8a003190e30d9f9833321ab925ee70
NadinShp <70705043+NadinShp@users.noreply.github.com>
2022-06-16 20:40:09 +0200

    brought back "suspense"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import 'modern-normalize/modern-normalize.css';
 import { Routes, Route } from 'react-router-dom';
-import CountryPage from './containers/CountryPage';
 import Header from './components/Header';
 import {getCountries} from './containers/MainPage/actions';
-import { lazy, memo, useEffect } from 'react';
+import {Suspense, lazy, memo, useEffect } from 'react';
 import {useDispatch} from 'react-redux';
 import {useState} from 'react';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme, GlobalStyles } from './theme';
 import {chooseTheme} from './containers/MainPage/actions';
+import Loader from './components/Loader';
 import './global/styles/index.css';
 
 const NotFound = lazy(() => import('./containers/NotFound'));
 const MainPage = lazy(() => import ('./containers/MainPage'));
+const CountryPage = lazy(() => import('./containers/CountryPage'));
 
 function App() {
   const dispatch = useDispatch();
@@ -33,11 +34,13 @@ function App() {
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyles />
       <Header onClick={toggleTheme} isDark={theme==='dark'}/>
-      <Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
           <Route path = "/" element = {<MainPage />} />
-          <Route path = ":countryId" element = {<CountryPage />} />
+          <Route path = "/:countryId" element = {<CountryPage />} />
           <Route path = "*" element = {<NotFound />} />
         </Routes>
+      </Suspense>
     </ThemeProvider>
   );
 }
--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -2,23 +2,16 @@ import { useSelector} from 'react-redux';
 import mainPageSelectors from './selectors';
 import CountryList from '../../components/CountryList';
 import Container from '../../components/Container';
-import Loader from '../../components/Loader';
 import Error from '../../components/Error';
 import AppBar from '../../components/AppBar';
 
 const MainPage = () => {
     const countries = useSelector(mainPageSelectors.countries);
-    const loader = useSelector(mainPageSelectors.loading);
     const error = useSelector(mainPageSelectors.error);
 
     return(
         <Container>
-            {loader && (<Loader />)}
             {error && (<Error message='Something is wrong. Try later' error={504}/>)}
-                {/* <div>
-                    <h>Something is wrong</h>
-                </div>
-            )} */}
             {countries && (
             <>
                 <AppBar />
